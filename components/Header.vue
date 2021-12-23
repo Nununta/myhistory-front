98713ad9975b0eb8acfbdab8fa1f3e83d7ff29e9
Nununta <yutayuki09040827@gmail.com>
2021-12-23 22:43:41 +0900

    a

---
+++ components/Header.vue
@@ -0,0 +1,62 @@
+<template>
+  <header>
+    <v-app-bar
+      app
+      color="cyan darken-4 indigo--text text--lighten-5"
+      dark
+      clipped-left
+    >
+      <v-toolbar-title>
+        <span class="pl-3">MyHistory</span>
+      </v-toolbar-title>
+      <!-- <v-tabs>
+                <v-tab to="/" v-if="isLogin"
+                    ><v-icon class="me-2">mdi-electron-framework</v-icon> TASK
+                </v-tab>
+                <v-tab to="/learn" v-if="isLogin"
+                    ><v-icon class="me-2">mdi-book-open-page-variant</v-icon>
+                    LEARN
+                </v-tab>
+                <v-tab v-if="isLogin" @click="logout"> Logout </v-tab>
+            </v-tabs> -->
+    </v-app-bar>
+  </header>
+</template>
+<script>
+//import { mapState } from "vuex";
+export default {
+  data() {
+    return {};
+  },
+  // methods: {
+  //     async logout() {
+  //         await this.$store.dispatch("auth/logout");
+
+  //         if (this.apiStatus) {
+  //             this.$router.push("/login");
+  //         }
+  //     },
+  // },
+  // computed: {
+  //     isLogin() {
+  //         return this.$store.getters["auth/check"];
+  //     },
+  //     username() {
+  //         return this.$store.getters["auth/username"];
+  //     },
+  //     ...mapState({
+  //         apiStatus: (state) => state.auth.apiStatus,
+  //     }),
+  // },
+};
+</script>
+<style scoped>
+.v-toolbar__title {
+  overflow: visible !important;
+  margin-right: 50px !important;
+}
+
+.v-application a {
+  color: hsla(0, 0%, 100%, 0.6);
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,165 @@
 <template>
-  <div>
-    <p @click="apitest">{{ posts.hello }}</p>
-  </div>
+  <v-card>
+    <v-tabs v-model="tabs" centered color="white">
+      <v-tab> Login </v-tab>
+      <v-tab> Register </v-tab>
+    </v-tabs>
+    <v-tabs-items v-model="tabs" class="mt-3">
+      <v-tab-item>
+        <v-form class="form" @submit.prevent="login">
+          <!-- エラー結果表示 -->
+          <!-- <div v-if="loginErrors" class="errors">
+          <ul v-if="loginErrors.email">
+            <li v-for="msg in loginErrors.email" :key="msg">
+              {{ msg }}
+            </li>
+          </ul>
+          <ul v-if="loginErrors.password">
+            <li v-for="msg in loginErrors.password" :key="msg">
+              {{ msg }}
+            </li>
+          </ul>
+        </div> -->
+          <v-card>
+            <v-text-field
+              label="Email"
+              class="mx-auto pt-0"
+              width="100%"
+              v-model="loginForm.email"
+            ></v-text-field>
+
+            <v-text-field
+              type="password"
+              label="Password"
+              class="mx-auto pt-0"
+              width="100%"
+              v-model="loginForm.password"
+            ></v-text-field>
+            <v-btn class="d-flex mx-auto mb-3 px-10" type="submit">
+              login
+            </v-btn>
+          </v-card>
+        </v-form>
+      </v-tab-item>
+      <v-tab-item>
+        <v-form class="form" @submit.prevent="register">
+          <v-card>
+            <!-- エラー結果表示 -->
+            <!-- <div v-if="registerErrors" class="errors">
+          <ul v-if="registerErrors.name">
+            <li v-for="msg in registerErrors.name" :key="msg">
+              {{ msg }}
+            </li>
+          </ul>
+          <ul v-if="registerErrors.email">
+            <li v-for="msg in registerErrors.email" :key="msg">
+              {{ msg }}
+            </li>
+          </ul>
+          <ul v-if="registerErrors.password">
+            <li v-for="msg in registerErrors.password" :key="msg">
+              {{ msg }}
+            </li>
+          </ul>
+        </div> -->
+            <label for="username">Name</label>
+            <input
+              type="text"
+              class="form__item"
+              id="username"
+              v-model="registerForm.name"
+            />
+            <label for="email">Email</label>
+            <input
+              type="text"
+              class="form__item"
+              id="email"
+              v-model="registerForm.email"
+            />
+            <label for="password">Password</label>
+            <input
+              type="password"
+              class="form__item"
+              id="password"
+              v-model="registerForm.password"
+            />
+            <label for="password-confirmation">Password (confirm)</label>
+            <input
+              type="password"
+              class="form__item"
+              id="password-confirmation"
+              v-model="registerForm.password_confirmation"
+            />
+            <div class="form__button">
+              <button type="submit" class="button button--inverse">
+                register
+              </button>
+            </div>
+          </v-card>
+        </v-form>
+      </v-tab-item>
+    </v-tabs-items>
+  </v-card>
 </template>
+
 <script>
-import axios from "axios";
+//import { mapState } from "vuex";
 export default {
-  components: {},
   data() {
     return {
-      dataTest: "",
+      tabs: null,
+      loginForm: {
+        email: "",
+        password: "",
+      },
+      registerForm: {
+        name: "",
+        email: "",
+        password: "",
+        password_confirmation: "",
+      },
     };
   },
-  async asyncData({ $axios }) {
-    const posts = await $axios.$get("/api/hello");
-    console.log($axios);
-    return { posts };
-  },
   methods: {
-    async apitest() {
-      const res = await axios.get("/api/hello");
-      console.log(res);
+    async login() {
+      // authストアのloginアクションを呼び出す
+      console.log("login");
+      // await this.$store.dispatch("auth/login", this.loginForm);
+
+      // if (this.apiStatus) {
+      //     // トップページに移動する
+      //     this.$router.push("/");
+      // }
     },
+    // async register() {
+    //     // authストアのresigterアクションを呼び出す
+    //     await this.$store.dispatch("auth/register", this.registerForm);
+
+    //     if (this.apiStatus) {
+    //         // トップページに移動する
+    //         this.$router.push("/");
+    //     }
+    // },
+    // clearError() {
+    //     this.$store.commit("auth/setLoginErrorMessages", null);
+    //     this.$store.commit("auth/setRegisterErrorMessages", null);
+    // },
   },
+  // computed: {
+  //     ...mapState({
+  //         apiStatus: (state) => state.auth.apiStatus,
+  //         loginErrors: (state) => state.auth.loginErrorMessages,
+  //         registerErrors: (state) => state.auth.registerErrorMessages,
+  //     }),
+  // },
+  // created() {
+  //     this.clearError();
+  // },
 };
 </script>
+
+<style>
+li {
+  list-style: none;
+}
+</style>
