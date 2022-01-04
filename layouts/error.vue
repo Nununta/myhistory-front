a4762091033febb38d4d33a290c31f83c392d5fc
Nununta <yutayuki09040827@gmail.com>
2022-01-04 23:48:23 +0900

    a

--- layouts/error.vue
+++ layouts/error.vue
@@ -6,36 +6,35 @@
     <h1 v-else>
       {{ otherError }}
     </h1>
-    <NuxtLink to="/">
-      Home page
-    </NuxtLink>
+    <NuxtLink to="/" v-if="$auth.loggedIn">Topページへ</NuxtLink>
+    <NuxtLink to="/" v-else>ログインページへ</NuxtLink>
   </v-app>
 </template>
 
 <script>
 export default {
-  name: 'EmptyLayout',
-  layout: 'empty',
+  name: "EmptyLayout",
+  layout: "empty",
   props: {
     error: {
       type: Object,
-      default: null
-    }
+      default: null,
+    },
   },
-  data () {
+  data() {
     return {
-      pageNotFound: '404 Not Found',
-      otherError: 'An error occurred'
-    }
+      pageNotFound: "お探しのページは見つかりませんでした",
+      otherError: "システムエラーが発生しました",
+    };
   },
-  head () {
+  head() {
     const title =
-      this.error.statusCode === 404 ? this.pageNotFound : this.otherError
+      this.error.statusCode === 404 ? this.pageNotFound : this.otherError;
     return {
-      title
-    }
-  }
-}
+      title,
+    };
+  },
+};
 </script>
 
 <style scoped>
